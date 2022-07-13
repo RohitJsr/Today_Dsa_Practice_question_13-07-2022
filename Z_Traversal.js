da4bd0b0c72ecb530bb4120097bbd2de3ff3b14f
RohitJsr <102182218+RohitJsr@users.noreply.github.com>
2022-07-13 23:37:10 +0530

    Add files via upload

---
+++ Z_Traversal.js
@@ -0,0 +1,52 @@
+// Z Traversal Ended
+// Description
+
+// Given a square matrix of size N x N. Print the Z traversal of the matrix. Refer the figure given below for better understanding.
+
+// Image
+
+
+// Input
+// The first line of the input contains T, the number of test cases. The first line of each test case contains N, the dimension of the square matrix.
+
+// Next N lines contains N space separated integers, denoting the values of the matrix.
+
+// Constraints
+
+// 1 <= T <= 10
+
+// 1 <= N <= 500
+
+// 1 <= A[i][j] <= 1000
+
+
+// Output
+// For each test case, print the elements that occur in the Z traversal of the matrix, on a new line.
+
+
+// Sample Input 1 
+
+// 1
+// 3
+// 1 2 3
+// 4 5 6
+// 7 8 9
+// Sample Output 1
+
+// 1 2 3 5 7 8 9 
+
+function zTraversal(N,arr){
+    //write code here
+    let inline=[];
+    for(let i=0;i<N-1;i++){
+        inline.push(arr[0][i])
+    }
+    for(let i=0,j=N-1;i<=N-1&&j>=0;i++,j--){
+        inline.push(arr[i][j])
+    }
+    for(let i=1;i<=N-1;i++){
+        inline.push(arr[N-1][i])
+    }
+    console.log(inline.join(" "))
+  
+}
